68a45f291500b5cd222d5d4612b0340c83ac795e
Savvas Sampaziotis <savvas456pentium@gmail.com>
2018-09-10 23:13:32 +0300

    mat2txt conversion function

--- matlab/mat2txt.m
+++ matlab/mat2txt.m
@@ -1,19 +1,17 @@
 
-clear
+function [] = mat2txt(I, filename)
 
-load('house.mat')
-Im = house;
-filename = 'house';
-
-% filename = 'test';
-% Im = reshape( 1:16, 4, 4)
-
+% clear
+% load('house.mat')
+% I = J;
+% filename = 'house';
 
 DATA_DIR = '../data/';
 fid = fopen(strcat(DATA_DIR , filename ,'.dat'), 'w');
-fwrite(fid, size(Im), 'int' );
-fwrite(fid, Im, 'float');
+fwrite(fid, size(I), 'int' );
+fwrite(fid, I, 'float');
 fclose(fid);
 
 % pixels are saved in float precision, because my machine runs CUDA 1.1, 
 % supporting only single precision floating point arithmetic
+end
